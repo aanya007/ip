b9c173ee020ab74af4021ad46fef01a8eacac0b8
aanya007 <aanya2106007@gmail.com>
2026-08-19 23:29:58 +0800

    Rename Duke template to Murphy

---
+++ src/main/java/Murphy.java
@@ -0,0 +1,10 @@
+public class Murphy {
+    public static void main(String[] args) {
+        String banner = "M   M  U   U  RRRR   PPPP   H   H  Y   Y\n"
+                + "MM MM  U   U  R   R  P   P  H   H   Y Y\n"
+                + "M M M  U   U  RRRR   PPPP   HHHHH    Y\n"
+                + "M   M  U   U  R  R   P      H   H    Y\n"
+                + "M   M   UUU   R   R  P      H   H    Y\n";
+        System.out.println(banner);
+    }
+}
