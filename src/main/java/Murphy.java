9dd15c2d8417632d0ebc07126e9052581abcc034
aanya007 <aanya2106007@gmail.com>
2026-08-19 23:35:39 +0800

    Add Murphy greeting and farewell messages

--- src/main/java/Murphy.java
+++ src/main/java/Murphy.java
@@ -1,10 +1,16 @@
 public class Murphy {
     public static void main(String[] args) {
+        String separator = "____________________________________________________________";
         String banner = "M   M  U   U  RRRR   PPPP   H   H  Y   Y\n"
                 + "MM MM  U   U  R   R  P   P  H   H   Y Y\n"
                 + "M M M  U   U  RRRR   PPPP   HHHHH    Y\n"
                 + "M   M  U   U  R  R   P      H   H    Y\n"
                 + "M   M   UUU   R   R  P      H   H    Y\n";
-        System.out.println(banner);
+
+        System.out.println(separator);
+        System.out.println(banner + "Hi there! I'm Murphy.\nWhat can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
